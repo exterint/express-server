7353f4f019e33deb63fce0b754fff3830c8b9a57
exterint <exterint@gmail.com>
2016-10-09 21:23:28 +0530

    Add routes

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', function(req, res) {
 	res.send('Hello World!');
 })
 
+app.get('/:name', function(req,res) {
+	res.send('Hello ' + req.params.name + '!');
+})
+
+app.get('/about', function(req,res) {
+	res.send('About Me!');
+})
+
 app.listen(server_port, function() {
 	console.log('Listening on port : ' + server_port);
 })
